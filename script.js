109eb065ecfbea5ec21064a6f737acf50730ca6e
Imstillsyncs <tjrobinson693@bullochschools.org>
2022-04-11 14:08:40 -0400

    4 files modified

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ console.log('updated!');
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
